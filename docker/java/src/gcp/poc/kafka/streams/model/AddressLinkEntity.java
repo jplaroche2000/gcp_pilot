c8d8fa4e3beb27ca531c01be4f1c35ca38737eda
Duke2 <jplaroche_2000@yahoo.ca>
2020-04-16 00:41:13 -0400

    initial commit

---
+++ docker/java/src/gcp/poc/kafka/streams/model/AddressLinkEntity.java
@@ -0,0 +1,154 @@
+package gcp.poc.kafka.streams.model;
+
+import org.json.JSONObject;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+
+public class AddressLinkEntity extends FirestoreEntity {
+
+	private Key key;
+	private Integer id;
+	private Integer addressId;	
+	private Integer customerId;
+	private String json;
+
+	
+	public AddressLinkEntity(JSONObject data) {
+		if(data.has("ID")) {
+			id = data.getInt("ID");
+		}
+		if (data.has("ADDRESS_ID")) {
+			addressId = data.getInt("ADDRESS_ID");
+		}
+		if(data.has("CUSTOMER_ID")) {
+			customerId = data.getInt("CUSTOMER_ID");
+		}
+		
+		this.json = data.toString();
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+
+
+
+
+	public Key getKey() {
+		return key;
+	}
+
+
+
+
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+
+
+	public Integer getAddressId() {
+		return addressId;
+	}
+
+
+
+
+
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
+	}
+
+
+
+
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+
+
+
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+
+
+
+
+	@Override
+	public Entity newEntity(Datastore datastore) {
+		
+		Entity customerEntity = Entity.newBuilder(datastore.newKeyFactory()
+			    .setKind("AddressLink")
+			    .newKey(id))
+			    .set("ADDRESS_ID", addressId)
+			    .set("CUSTOMER_ID", customerId)
+			    .build();
+		
+		return customerEntity;
+	}
+
+
+	@Override
+	public Key getKey(Datastore datastore) {
+		return datastore.newKeyFactory()
+			    .setKind("AddressLink")
+			    .newKey(id);
+	}
+
+
+	@Override
+	public Entity updatedEntity(Datastore datastore) {
+		Key key = datastore.newKeyFactory()
+	    .setKind("AddressLink")
+	    .newKey(id);
+		
+		Entity entity = datastore.get(key);
+		
+		Entity.Builder entityBuilder = Entity.newBuilder(entity);
+		
+		if(id != null) {
+			entityBuilder.set("ID", id);
+		}
+		if (customerId != null) {
+			entityBuilder.set("CUSTOMER_ID", customerId);
+		}
+		if (addressId != null) {
+			entityBuilder.set("ADDRESS_ID", addressId);
+		}
+
+		return entityBuilder.build();
+		
+	}
+	
+	@Override
+	public String getNamespace() {
+		return "/" + this.getClass().getCanonicalName() + "/" + id.toString();
+	}
+}
